069f242499565e8b422dfe3f60635e933cbe2eb8
Jaemin Kim <huple.jaemin@gmail.com>
2022-05-13 10:20:29 +0900

    Initial Commit

---
+++ TinyTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TinyTest
+//
+//  Created by 김재민 on 2022/05/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TinyTest/TinyTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinyTestApp.swift
+//  TinyTest
+//
+//  Created by 김재민 on 2022/05/13.
+//
+
+import SwiftUI
+
+@main
+struct TinyTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
